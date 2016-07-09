065bd84a030c9b9bf077898a1e6dbae508b8b458
billrozy <you@example.com>
2016-07-09 17:06:59 +0300

    progressBar added

--- Soundbeam/app/src/main/java/com/example/soundbeam/MainActivity.java
+++ Soundbeam/app/src/main/java/com/example/soundbeam/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.soundbeam;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -10,13 +9,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -27,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private ProgressBar mProgressBar;
     private Context mContext;
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,17 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 if(resultCode == RESULT_OK){
                     Uri selectedImage = imageReturnedIntent.getData();
                     new LoadImageTask().execute(selectedImage);
-                    /*
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    imageView.setImageBitmap(bitmap);
-                    sections= sectionMaker(bitmap, 8);
                     TextView textView = (TextView)findViewById(R.id.textView);
-                   // textView.setText(av[0]+"  "+av[1]+" "+av[2]+" "+av[3]);
-                   */
                 }
         }
     }
@@ -174,6 +164,71 @@ public class MainActivity extends AppCompatActivity {
             mProgressBar.setProgress(100);
             mProgressBar.setVisibility(View.INVISIBLE);
             imageView.setImageBitmap(bitmap);
+            new MakeSectionsTask().execute(bitmap);
+            mProgressBar.setProgress(0);
+
+        }
+    }
+
+    private class MakeSectionsTask extends AsyncTask<Bitmap, Integer, Integer> {
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setProgress(0);
+            mProgressBar.setVisibility(View.VISIBLE);
+            super.onPreExecute();
+        }
+
+        protected Integer doInBackground(Bitmap... map) {
+            int count = map.length;
+            int num = 8;
+            long totalSize = 0;
+            for (int i = 0; i < count; i++) {
+                //sections= sectionMaker(map[i], 8);
+                sections = new Section[num];
+                int A, R, G, B;
+
+                int pixelColor;
+                int width = map[i].getWidth();
+                int height = map[i].getHeight();
+                int size = width * height;
+                int widthOfSection = width/num;
+                int sizeOfSection = widthOfSection * height;
+
+                for (int s = 0; s < num; s++) {
+                    Section current = new Section();
+                    A = R = G = B = 0;
+                    for (int x = widthOfSection*s; x < widthOfSection*s + widthOfSection; ++x) {
+                        for (int y = 0; y < height; ++y) {
+                            pixelColor = map[i].getPixel(x, y);
+                            A += Color.alpha(pixelColor);
+                            R += Color.red(pixelColor);
+                            G += Color.green(pixelColor);
+                            B += Color.blue(pixelColor);
+                        }
+                    }
+                    // current.setStart(widthOfSection*s);
+                    current.setA(A/sizeOfSection);
+                    current.setR(R/sizeOfSection);
+                    current.setG(G/sizeOfSection);
+                    current.setB(B/sizeOfSection);
+                    sections[s] = current;
+                    publishProgress(s++);
+                }
+                Section.WIDTH=widthOfSection;
+                Section.HEIGHT=height;
+            }
+            return 1;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            //setProgressPercent(progress[0]);
+            mProgressBar.setProgress(progress[0]);
+
+        }
+
+        protected void onPostExecute(Integer num) {
+            mProgressBar.setProgress(100);
+            mProgressBar.setVisibility(View.INVISIBLE);
             mProgressBar.setProgress(0);
 
         }
